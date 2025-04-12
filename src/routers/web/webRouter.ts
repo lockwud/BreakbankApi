97de53e2f3c4e33d1d8e33f13a3fec9a264fe564
lockwud <oklement3@gmail.com>
2025-04-12 20:59:42 +0000

    Added routes

--- src/routers/web/webRouter.ts
+++ src/routers/web/webRouter.ts
@@ -1,12 +1,10 @@
 import {Router } from "express"
 const webRouter = Router();
-import authRoute from "./auth.route";
-import reportRoute from "./itemReport.route";
 
 
 
-webRouter.use("/auth", authRoute)
-webRouter.use("/report", reportRoute)
+webRouter.use()
+webRouter.use()
 
 
 
