2593026a0a591163ea8968828af04e9159ab091b
lockwud <oklement3@gmail.com>
2025-04-13 03:48:41 +0000

    Updated the route

--- src/routers/web/webRouter.ts
+++ src/routers/web/webRouter.ts
@@ -1,11 +1,12 @@
 import {Router } from "express"
 import authRoute from "./auth.route";
+import uploadRoute from "./fileupload.route";
 const webRouter = Router();
 
 
 
 webRouter.use("/auth", authRoute)
-webRouter.use("/question")
+webRouter.use("/question", uploadRoute)
 
 
 
