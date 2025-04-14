b5051a7d2df58c307095a754e4e190490a0c8866
lockwud <oklement3@gmail.com>
2025-04-14 21:16:23 +0000

    Fixing cors error when uploading file

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,11 @@ const app: Express = express();
 const port = process.env.PORT || 4500;
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://break-bank.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Range", "X-Content-Range"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    maxAge: 86400,
-  })
-);
+app.use(cors({
+  origin: 'https://break-bank.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
